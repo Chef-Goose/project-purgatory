63bf3ea4fbb7f5547d37ee3f41d245f2ab3507e0
Chef-Goose <150098211+Chef-Goose@users.noreply.github.com>
2026-03-21 15:16:40 -0600

    connecting sounds

---
+++ scripts/menu/menu_button.gd
@@ -0,0 +1,27 @@
+extends Button
+
+@export var hover_sound: AudioStream = preload("res://assets/audio/Menu Up-Down.wav")
+@export var pressed_sound: AudioStream = preload("res://assets/audio/Menu Select Sound.wav")
+
+@onready var _audio_player: AudioStreamPlayer = $AudioStreamPlayer
+
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	pressed.connect(_on_pressed)
+
+
+func _on_mouse_entered() -> void:
+	_play_sound(hover_sound)
+
+
+func _on_pressed() -> void:
+	_play_sound(pressed_sound)
+
+
+func _play_sound(sound: AudioStream) -> void:
+	if sound == null:
+		return
+
+	_audio_player.stream = sound
+	_audio_player.play()
